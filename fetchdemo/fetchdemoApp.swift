80855730d5cf3a499a088e5b47110b9630f7c5f3
Chika Ohaya <chika.ohaya@gmail.com>
2023-03-10 15:22:43 -0500

    Initial Commit

---
+++ fetchdemo/fetchdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchdemoApp.swift
+//  fetchdemo
+//
+//  Created by Chika Ohaya on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct fetchdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
